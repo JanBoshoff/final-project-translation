e0cf15541d35bb49b618d174124222c33c842e36
JanBoshoff <boshoffjanl@gmail.com>
2023-01-06 16:40:44 +0800

    Integrated IBM Watson translator to finish translation functions

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,22 +2,26 @@ import unittest
 
 from translator import english_to_french, french_to_english
 
-class TestFrenchToEnglish(unittest.TestCase): 
-    def test1(self): 
-        self.assertRaises(Exception, french_to_english, None) # test when None is given as input, the function raises an exception.
 
+class TestFrenchToEnglish(unittest.TestCase):
 
-    def test2(self):
-        self.assertEqual(french_to_english('Bonjour'), 'Hello') # test when Bonjour is given as input, the output is Hello
-
-        
-
-class TestEnglishToFrench(unittest.TestCase): 
-    def test1(self): 
-        self.assertRaises(Exception, english_to_french, None) # test when None is given as input, the function raises an exception.
+    def test1(self):
+        # test when None is given as input, the function raises an exception.
+        self.assertRaises(Exception, french_to_english, None)
 
     def test2(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour') # test when Hello is given as input, the output is Bonjour
+        # test when Bonjour is given as input, the output is Hello
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
-        
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        # test when None is given as input, the function raises an exception.
+        self.assertRaises(Exception, english_to_french, None)
+
+    def test2(self):
+        # test when Hello is given as input, the output is Bonjour
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,33 +1,86 @@
-import os
-import json
+"""
+This module has functions to translate strings between English and French.
+"""
 
-from ibm_watson import LanguageTranslatorV3
+import os
+
+from ibm_watson import LanguageTranslatorV3, ApiException
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apikey = os.environ['apikey']
-url = os.environ['url']
+apiurl = os.environ['url']
+version = os.environ['version']
 
-def english_to_french (english_text: str) -> str:
-	"""
-	This function takes an English str as input and returns the equivalent French as a string.
-	"""
-	_check_input(english_text)
+auth = IAMAuthenticator(apikey)
+lang_translator = LanguageTranslatorV3(
+    version="2018-05-01",
+    authenticator=auth
+)
+
+lang_translator.set_service_url(apiurl)
 
 
+class TranslationFailedException (Exception):
+    """
+    Exception raised when the translation API failed.
 
-	return ''
+    Attributes:
+        code -- the error code from the API
+        message -- a corresponding message
+    """
 
-def french_to_english (french_text: str) -> str:
-	"""
-	This function takes a French string as input and returns the equivalent English as a string.
-	"""
-	_check_input(french_text)
+    def __init__(self, code, message):
+        self.code = code
+        self.message = f'Could not translate - api error [{code}] -> {message}'
 
-	return ''
+        super().__init__(self.message)
 
-def _check_input (input_val: str):
-	if input_val is None:
-		raise Exception('Input value can not be None')
+
+def english_to_french(english_text: str) -> str:
+    """
+    This function takes an English str as input and
+    returns the equivalent French as a string.
+    """
+    _check_input(english_text)
+
+    if english_text == '':
+        return ''
+
+    return _translate(lang_translator, english_text, 'en', 'fr')
+
+
+def french_to_english(french_text: str) -> str:
+    """
+    This function takes a French string as input and
+    returns the equivalent English as a string.
+    """
+    _check_input(french_text)
+
+    if french_text == '':
+        return ''
+
+    return _translate(lang_translator, french_text, 'fr', 'en')
+
+
+def _check_input(input_val: str):
+    if input_val is None:
+        raise Exception('Input value can not be None')
+
+
+def _translate(translator: LanguageTranslatorV3, text: str, source_lang_id: str, target_lang_id: str) -> str:
+    translation = translator.translate(
+        text=text,
+        model_id=f'{source_lang_id}-{target_lang_id}'
+    )
+
+    translated_text = ''
+    try:
+        result = translation.get_result()
+        translated_text = result.get('translations')[0].get('translation')
+    except ApiException as ex:
+        raise TranslationFailedException(ex.code, ex.message) from ex
+
+    return translated_text
